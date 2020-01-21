2ebdad0604f7abc5b6d27950f5e14101764794b2
ble9 <brianle42@live.com>
2020-01-21 00:09:48 -0600

    adding auth

--- functions/prodadmin/js/login_page.js
+++ functions/prodadmin/js/login_page.js
@@ -8,9 +8,26 @@ function login_page() {
         <h3>  Please Sign In</h3>
           <input type="email" class="form-control" id="email" placeholder ="Email address">
           <input type="password" class="form-control" id="password" placeholder ="Password">
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary" onclick="signIn()">Submit</button>
       </form>`;
     }
-
   })
+}
+
+function signIn() {
+  try {
+    const email = document.getElementById('email').value
+    if (email !== 'prodcadmin@test.com') {
+      throw new Error('Not product admin')
+    }
+    const password = document.getElementById('password').value
+    await firebase.auth().signInWithEmailAndPassword(email, password)
+    //go to admin home 
+    window.location.href = '/home'
+  } catch (e) {
+    glPageContent = 'Lgin Failed: <br>' + e + '<br>'
+    glPageContent += '< a href = "/login" class =  btn btn-outline-primary"> Go To Login </a>'
+
+  }
+
 }
